253af6ea1e9f3df718757b36485430075c6b24ad
OliveiraGusta <gustavo.oliver456@gmail.com>
2024-07-25 12:41:20 -0300

    Criado End point getById e getByTitle

--- src/main/java/com/generation/blogpessoal/controller/PostController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostController.java
@@ -3,9 +3,11 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,16 @@ public class PostController {
 	public ResponseEntity<List<Post>> getall(){
 		return ResponseEntity.ok(postRepository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> getById(@PathVariable Long id){
+		return postRepository.findById(id)
+				.map(response -> ResponseEntity.ok(response))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
+	@GetMapping("/title/{title}")
+	public ResponseEntity<List<Post>> getByTitle(@PathVariable String title){
+		return ResponseEntity.ok(postRepository.findAllByTitleContainingIgnoreCase(title));
+	}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.generation.blogpessoal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import com.generation.blogpessoal.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+	public List<Post> findAllByTitleContainingIgnoreCase(@Param("title") String title);
+	
 }
